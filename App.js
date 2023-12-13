914810eaba35a3912eefa260f26995c5f408ff10
syedtabeeb1992 <syedtabeeb@gmail.com>
2023-12-13 10:48:36 +0530

    all basic functiosn working

--- App.js
+++ App.js
@@ -14,13 +14,12 @@ import {
 const App = () => {
   const [householditems, sethouseholditems] = useState([]);
   const [formData, setFormData] = useState({
-    name: '',
-    quantity: '',
+    name: '', // Initialize with appropriate default values
+    quantity: 0,
     boughtdate: '',
     expirydate: '',
     veg: true, // Default to veg
   });
-
   
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -90,19 +89,68 @@ const App = () => {
     getdata();
   }, []);
 
+  const edit = (response) => {
+    console.log("BOBO", response);
+  
+    setFormData((prevData) => ({
+      ...prevData,
+      name: response.name || prevData.name,
+      quantity: response.quantity || prevData.quantity,
+      boughtdate: response.boughtdate || prevData.boughtdate,
+      expirydate: response.expirydate || prevData.expirydate,
+      veg: response.veg !== undefined ? response.veg : prevData.veg,
+    }));
+  }
+
+  const update = async () => {
+    console.log("UPDATED");
+    try {
+      if (!formData.name) {
+        console.error("Name is required for update");
+        return;
+      }
+
+      const docRef = doc(db, "householditems", "7lJo4W3RGRuZ9zL5a4FW");
+
+      // Find the index of the category to be updated
+      const categoryIndex = householditems.findIndex(
+        (item) => item.categories.some((category) => category.name === formData.name)
+      );
+
+      if (categoryIndex === -1) {
+        console.error("Category not found for update");
+        return;
+      }
+
+      // Update the category in Firestore
+      await updateDoc(docRef, {
+        categories: arrayRemove(householditems[categoryIndex].categories.find(category => category.name === formData.name)),
+      });
+
+      await updateDoc(docRef, {
+        categories: arrayUnion(formData),
+      });
+
+      // Update the state to reflect the changes in the UI
+      sethouseholditems((prevItems) => {
+        const updatedItems = [...prevItems];
+        updatedItems[categoryIndex].categories = updatedItems[categoryIndex].categories.map((category) =>
+          category.name === formData.name ? formData : category
+        );
+        return updatedItems;
+      });
+
+      console.log("Category updated successfully");
+    } catch (error) {
+      console.error("Error updating category: ", error);
+    }
+  };
+
+
   return (
     <div>
-
-
-
-
-
-      <div className="" style={{border:"1px solid red", padding:"10px"}}>
-
-
-
-        <h1>Add Items</h1>
-
+      <div className="" style={{border:"1px solid green", padding:"10px"}}>
+        <h1>Add Itedddsssssms</h1>
       <input
         type="text"
         placeholder="Name of the item"
@@ -141,6 +189,7 @@ const App = () => {
       </select>
 
       <button onClick={handleSubmit}>Add Item</button>
+      <button onClick={update}>UPDATE</button>
  
 
       </div>
@@ -154,6 +203,7 @@ const App = () => {
                 <p>Expiring on - {response.expirydate}</p>
                 <p>Quantity - {response.quantity}</p>
                 <button onClick={() => deleteItems(response.name, items.id)}>Delete</button>
+                <button onClick={() => edit(response)}>EDIT</button>
               </div>
             );
           });
